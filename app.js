f6104ccddee2353d57af4f15053b0bfa000498a4
ArvidErik <erik.arvid.hosszu@gmail.com>
2023-08-30 12:30:05 +0200

    item route added

--- app.js
+++ app.js
@@ -18,8 +18,13 @@ require("./config")(app);
 const indexRoutes = require("./routes/index.routes");
 app.use("/api", indexRoutes);
 
+const itemRoutes = require("./routes/item.routes");
+app.use("/api", itemRoutes);
+
 const authRoutes = require("./routes/auth.routes");
 app.use("/auth", authRoutes);
+
+
 
 // ❗ To handle errors. Routes that don't exist or errors that you handle in specific routes
 require("./error-handling")(app);

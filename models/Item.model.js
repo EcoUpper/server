61ff3625cbff87beca1ef1a7f03cd9515e379c23
ArvidErik <erik.arvid.hosszu@gmail.com>
2023-08-30 11:59:14 +0200

    item model done

---
+++ models/Item.model.js
@@ -0,0 +1,44 @@
+const { Schema, model } = require("mongoose");
+
+// TODO: Please make sure you edit the User model to whatever makes sense in this case
+const itemSchema = new Schema(
+  {
+    name: {
+        type: String,
+        required: [true, "Name is required."]
+    },
+    description: {
+        type: String,
+        required: [true, "Description is required."]
+    },
+    image_url: {
+        type: String,
+        required: [true, "Image is required."]
+    },
+    type: {
+        type: String,
+        required: [true, "Type is required."],
+        enum: ["clothing", "food", "other"],
+        default: "other"
+    },
+    expiration_date: {
+        type: Date,
+        min: Date.now() + 7*24*60*60*1000,
+    },
+    proposals: [{ type: Schema.Types.ObjectId, ref: "Proposal" }],
+    status: {
+        type: String,
+        enum: ["available", "reserved", "gifted"],
+        default: "available"
+    },
+    owner: [{ type: Schema.Types.ObjectId, ref: "User" }]
+  },
+  {
+    // this second object adds extra properties: `createdAt` and `updatedAt`
+    timestamps: true,
+  }
+);
+
+const Item = model("Item", itemSchema);
+
+module.exports = Item;

b7d7fa1fe1b696a363aa302115df5683f3800402
Edna Vidal <ednavidalros@gmail.com>
2023-09-01 14:49:11 +0200

    "item model modified + location"

--- models/Item.model.js
+++ models/Item.model.js
@@ -1,37 +1,40 @@
 const { Schema, model } = require("mongoose");
 
-// TODO: Please make sure you edit the User model to whatever makes sense in this case
 const itemSchema = new Schema(
   {
     name: {
-        type: String,
-        required: [true, "Name is required."]
+      type: String,
+      required: [true, "Name is required."]
     },
     description: {
-        type: String,
-        required: [true, "Description is required."]
+      type: String,
+      required: [true, "Description is required."]
     },
     image_url: {
-        type: String,
-        required: [true, "Image is required."]
+      type: String,
+      required: [true, "Image is required."]
     },
     type: {
-        type: String,
-        required: [true, "Type is required."],
-        enum: ["clothing", "food", "other"],
-        default: "other"
+      type: String,
+      required: [true, "Type is required."],
+      enum: ["clothing", "food", "other"],
+      default: "other"
     },
     expiration_date: {
-        type: Date,
-        min: Date.now() + 7*24*60*60*1000,
+      type: Date,
+      min: Date.now() + 7 * 24 * 60 * 60 * 1000,
     },
     proposals: [{ type: Schema.Types.ObjectId, ref: "Proposal" }],
     status: {
-        type: String,
-        enum: ["available", "reserved", "gifted"],
-        default: "available"
+      type: String,
+      enum: ["available", "reserved", "gifted"],
+      default: "available"
     },
-    owner: { type: Schema.Types.ObjectId, ref: "User" }
+    owner: { type: Schema.Types.ObjectId, ref: "User" },
+    location: {
+      type: String,
+      required: [true, "Location is required."]
+    }
   },
   {
     timestamps: true,

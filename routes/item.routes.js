fd2fc42f2910947b50b48d6fe4df37f214353975
Edna Vidal <ednavidalros@gmail.com>
2023-09-01 14:58:55 +0200

    "Commit 3516, itemroute modified to fit new model"

--- routes/item.routes.js
+++ routes/item.routes.js
@@ -110,7 +110,7 @@ router.get("/items/owner/:userId", (req, res) => {
 
 router.post("/items/create/new", (req, res, next) => {
 
-    const {name, description, image_url, type, expiration_date, status, owner} = req.body
+    const {name, description, image_url, type, expiration_date, status, owner, location} = req.body
 
     const itemObj = {
         name: name,
@@ -119,7 +119,8 @@ router.post("/items/create/new", (req, res, next) => {
         type : type,
         expiration_date: expiration_date,
         status : status,
-        owner : owner
+        owner : owner,
+        location: location,
     }
 
     Items.create(itemObj)
